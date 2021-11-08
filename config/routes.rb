673a3fbd7ab6a87ab0482141d8a7b40362cc3689
imdad Adelabou <imdadadelabou0@gmail.com>
2021-11-08 10:51:56 +0100

    implement logout function

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   root 'sessions#new'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 end
