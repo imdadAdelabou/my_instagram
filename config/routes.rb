de8906fcdd02a787b1573f0935fd7364dc959a08
imdad Adelabou <imdadadelabou0@gmail.com>
2021-11-08 09:52:49 +0100

    currently desigining inscription design

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   root 'sessions#new'
-  resources :users
+  resources :users, only: [:new, :create]
 end
